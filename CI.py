13ea6496ce03a97cb508cf5b9b903839f1c71f76
Swathi .G <71583010+Swathi0313@users.noreply.github.com>
2022-06-09 11:55:50 +0530

    Update CI.py

--- CI.py
+++ CI.py
@@ -2,10 +2,10 @@ def CompoundInterest (p,r,t):
 	if t==0:
 		return p
 	else:
-		ci=p*(pow((1+r/100),t))
+		c=p*(pow((1+r/100),t))
 		return ci
 p=float(input("enter the principal amount"))
 r=float(input("enter the rate of interest"))
 t=float(input("enter the number of years"))
-ci=CompoundInterest(p,r,t)
-print("compound intest of {0} is {1:2.2f}".format(p,ci))
+c=CompoundInterest(p,r,t)
+print("compound interest is {0:5.2f}".format(c))
